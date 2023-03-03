15bef385d180694e8bc9fc1e60c740405916ebbc
herom <jerome.manset22@gmail.com>
2023-03-04 00:52:32 +0800

    Create Header component and add elements

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-blue-500 flex justify-between items-center p-4'>
+      <img src="/src/assets/images/logo.svg" alt="Blogr logo" />
+      <img src="/src/assets/images/icon-hamburger.svg" alt="Hamburger icon" />
+    </header>
+  )
+}
+
+export default Header
