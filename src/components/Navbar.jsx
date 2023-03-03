62c2ace458f10c19a9f9204576034cf706f9bbbf
herom <jerome.manset22@gmail.com>
2023-03-04 07:56:58 +0800

    Create states for sublists and a function that when the specific list is clicked it shows the sublists and hide if there is open sublists

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,34 +2,54 @@ import React, { useState } from 'react'
 
 const Navbar = (props) => {
 
-  const listOpen = false
+  const [isShowProduct, setIsShowProduct] = useState(false)
+  const [isShowCompany, setIsShowCompany] = useState(false)
+  const [isShowConnect, setIsShowConnect] = useState(false)
 
-    const arrowUp = <img className='inline' src="./src/assets/images/icon-arrow-dark.svg" alt="Icon arrow up" />
-    const arrowDown = <img className='inline' src="./src/assets/images/icon-arrow-light.svg" alt="Icon arrow up" />
+  const arrowUp = <img className='inline' src="./src/assets/images/icon-arrow-dark.svg" alt="Icon arrow up" />
+  const arrowDown = <img className='inline' src="./src/assets/images/icon-arrow-light.svg" alt="Icon arrow up" />
+
+  function toggleSublist(e){
+    const id = e.target.id
+    if(id === 'product'){
+      setIsShowProduct(prevState => !prevState)
+      setIsShowCompany(false)
+      setIsShowConnect(false)
+    }else if(id === 'company'){
+      setIsShowProduct(false)
+      setIsShowCompany(prevState => !prevState)
+      setIsShowConnect(false)
+    }else if(id === 'connect'){
+      setIsShowProduct(false)
+      setIsShowCompany(false)
+      setIsShowConnect(prevState => !prevState)
+    }
+  }
+
 
   return (
-    props.isMenuOpen && <nav className='mt-10 bg-white text-center rounded-lg leading-12 font-semibold text-darkblue text-xl z-20'>
-      <ul className='py-5 border-b border-gray-200' >
-        <li>Product {arrowUp}
-            {listOpen && <ul className='nav--sublists'>
+    props.isMenuOpen && <nav className='mt-10 bg-white text-center rounded-lg leading-12 font-semibold text-darkblue text-xl shadow-xl z-20'>
+      <ul className='nav--lists py-5 border-b border-gray-200'>
+        <li id="product" onClick={toggleSublist}>Product {arrowUp}
+        <ul className={`nav--sublists ${isShowProduct ? 'block' : 'hidden'}`}>
                 <li>Product 1</li>
                 <li>Product 2</li>
-                <li>Product 2</li>
-            </ul>}
+                <li>Product 3</li>
+            </ul>
         </li>
-        <li>Company {arrowUp}
-            {listOpen && <ul className='nav--sublists'>
+        <li id="company" onClick={toggleSublist}>Company {arrowUp}
+        <ul className={`nav--sublists ${isShowCompany ? 'block' : 'hidden'}`}>
                 <li>Company 1</li>
                 <li>Company 2</li>
-                <li>Company 2</li>
-            </ul>}
+                <li>Company 3</li>
+            </ul>
         </li>
-        <li>Connect {arrowUp}
-            {listOpen && <ul className='nav--sublists'>
+        <li id="connect" onClick={toggleSublist}>Connect {arrowUp}
+           <ul className={`nav--sublists ${isShowConnect ? 'block' : 'hidden'}`}>
                 <li>Contact</li>
                 <li>Newsletter</li>
                 <li>LinkedIn</li>
-            </ul>}
+            </ul>
         </li>
       </ul>
       <ul className='py-5'>
