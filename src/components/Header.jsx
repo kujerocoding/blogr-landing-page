1a71f6b42104ffb21dfb6020a3f4a15a353cac35
herom <jerome.manset22@gmail.com>
2023-03-08 05:28:40 +0800

    Add media query for nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,12 +12,15 @@ const Header = () => {
 
   return (
     <header className='absolute top-0 pt-10 w-full px-5 z-20 md:px-32 md:pt-12'>
-      <div className='flex justify-between items-center'>
-        <img src="/src/assets/images/logo.svg" alt="Blogr logo" />
-        <img className='md:hidden' src={`/src/assets/images/${!isMenuOpen ? 'icon-hamburger' : 'icon-close'}.svg`} 
+      <div className='grid grid-cols-2 md:flex md:justify-between md:items-center md:gap-10'>
+      
+        <img className='cursor-pointer' src="/src/assets/images/logo.svg" alt="Blogr logo" />
+        
+        <img className='md:hidden place-self-end self-center cursor-pointer' src={`/src/assets/images/${!isMenuOpen ? 'icon-hamburger' : 'icon-close'}.svg`} 
         alt={`${!isMenuOpen ? 'Hamburge' : 'X'}icon`} onClick={toggleMenu}/>
+       <Navbar isMenuOpen={isMenuOpen}/>
       </div>
-      <Navbar isMenuOpen={isMenuOpen}/>
+      
     </header>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,22 +29,27 @@ const Navbar = (props) => {
 
 
   return (
-    props.isMenuOpen && <nav className='mt-10 bg-white text-center rounded-lg leading-12 font-semibold text-darkblue text-xl shadow-xl z-50'>
+     <nav className='bg-white text-center rounded-lg  font-semibold text-darkblue text-xl shadow-xl z-50 col-span-2 md:flex-1 md:bg-transparent md:shadow-none md:text-white'>
+
+      {props.isMenuOpen && <div className='leading-12 md:hidden'>
       <ul className='nav--lists py-5 border-b border-gray-200'>
         <li id="product" onClick={toggleSublist}>Product&ensp;
         <img className={`inline ${isShowProduct ? 'rotate-180' : ""}`} src="./src/assets/images/icon-arrow-dark.svg" alt="Icon arrow" />
         <ul className={`nav--sublists ${isShowProduct ? 'block' : 'hidden'}`}>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
+                <li>Overview</li>
+                <li>Pricing</li>
+                <li>Marketplace</li>
+                <li>Features</li>
+                <li>Integrations</li>
             </ul>
         </li>
         <li id="company" onClick={toggleSublist}>Company&ensp;
         <img className={`inline ${isShowCompany ? 'rotate-180' : ""}`} src="./src/assets/images/icon-arrow-dark.svg" alt="Icon arrow" />
         <ul className={`nav--sublists ${isShowCompany ? 'block' : 'hidden'}`}>
-                <li>Company 1</li>
-                <li>Company 2</li>
-                <li>Company 3</li>
+                <li>About</li>
+                <li>Team</li>
+                <li>Blog</li>
+                <li>Careers</li>
             </ul>
         </li>
         <li id="connect" onClick={toggleSublist}>Connect&ensp;
@@ -60,6 +65,43 @@ const Navbar = (props) => {
         <li><button>Login</button></li>
         <li><button className='px-10 py-2 text-white rounded-full bg-gradient-to-l from-lightred to-verylightred'>Signup</button></li>
       </ul>
+      </div>}
+
+      <div className='hidden md:flex md:justify-between text-sm'>
+      <ul className='flex gap-10'>
+        <li className='nav__menu--link' id="product" onClick={toggleSublist}>Product&ensp;
+        <img className={`inline ${isShowProduct ? 'rotate-180' : ""}`} src="./src/assets/images/icon-arrow-light.svg" alt="Icon arrow" />
+        <ul className={`nav__menu--sublinks ${isShowProduct ? 'block' : 'hidden'}`}>
+                <li>Overview</li>
+                <li>Pricing</li>
+                <li>Marketplace</li>
+                <li>Features</li>
+                <li>Integrations</li>
+            </ul>
+        </li>
+        <li className='nav__menu--link' id="company" onClick={toggleSublist}>Company&ensp;
+        <img className={`inline ${isShowCompany ? 'rotate-180' : ""}`} src="./src/assets/images/icon-arrow-light.svg" alt="Icon arrow" />
+        <ul className={`nav__menu--sublinks ${isShowCompany ? 'block' : 'hidden'}`}>
+                <li>About</li>
+                <li>Team</li>
+                <li>Blog</li>
+                <li>Careers</li>
+            </ul>
+        </li>
+        <li className='nav__menu--link' id="connect" onClick={toggleSublist}>Connect&ensp;
+        <img className={`inline ${isShowConnect ? 'rotate-180' : ""}`} src="./src/assets/images/icon-arrow-light.svg" alt="Icon arrow" />
+           <ul className={`nav__menu--sublinks ${isShowConnect ? 'block' : 'hidden'}`}>
+                <li>Contact</li>
+                <li>Newsletter</li>
+                <li>LinkedIn</li>
+            </ul>
+        </li>
+      </ul>
+      <ul className='flex justify-center items-center gap-10 pl-6'>
+        <li><button>Login</button></li>
+        <li><button className='px-10 py-2 text-lightred rounded-full bg-white'>Signup</button></li>
+      </ul>
+      </div>
     </nav>
   )
 }
